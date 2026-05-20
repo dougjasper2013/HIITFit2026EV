93c8209ea37092952844832ed251525e8393a740
Douglas Jasper <doug.jasper@trios.com>
2026-05-20 19:34:07 -0400

    Added Arrays

--- HIITFit/Views/ContentView.swift
+++ HIITFit/Views/ContentView.swift
@@ -5,8 +5,8 @@ struct ContentView: View {
   var body: some View {
       VStack {
           TabView {
-              Text("Welcome!")
-              Text("Exercise 1")
+              WelcomeView() // was Text("Welcome!")
+              ExerciseView(index: 0) // was Text("Exercise 1")
               Text("Exercise 2")
           }
           .tabViewStyle(PageTabViewStyle())
