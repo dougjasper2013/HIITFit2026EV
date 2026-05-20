9d1f0edeb22abbaa1b06e016175a30c0e0417391
Douglas Jasper <doug.jasper@trios.com>
2026-05-20 18:59:27 -0400

    with TabItems

--- HIITFit/ContentView.swift
+++ HIITFit/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
               Text("Exercise 2")
                   .tabItem { Text("Exercise 2")}
           }
+          .tabViewStyle(PageTabViewStyle())
+          .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
       }
   }
 }
