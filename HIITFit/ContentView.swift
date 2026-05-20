c3418bd02e28304f2a977cf873e6fb16c80f0331
Douglas Jasper <doug.jasper@trios.com>
2026-05-20 18:47:28 -0400

    Initial Commit

---
+++ HIITFit/ContentView.swift
@@ -0,0 +1,21 @@
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+      VStack {
+          TabView {
+              Text("Welcome!")
+                  .tabItem { Text("Welcome")}
+              Text("Exercise 1")
+                  .tabItem { Text("Exercise 1")}
+              Text("Exercise 2")
+                  .tabItem { Text("Exercise 3")}
+          }
+      }
+  }
+}
+
+#Preview {
+  ContentView()
+}
